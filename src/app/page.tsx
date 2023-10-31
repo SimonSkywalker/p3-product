9a4ef1c9ee23ee26857cdd97e627c5d15fb84f48
simonthordal <sthordal@gmail.com>
2023-10-31 12:42:45 +0100

    Co-authored-by: Skjodt <Skjodt@users.noreply.github.com> Co-authored-by: Allan Vester <avest22@student.aau.dk> Co-authored-by: AllanVester <AllanVester@users.noreply.github.com> Added first pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      
+      <h1>Welcome to Project survey tool</h1>
+      <div className='flex flex-wrap content-evenly'>
+        <Link className="border-white border-2 p-2" href={"/leaderHome"}>LH</Link><br />
+        <Link className="border-white border-2 p-2" href={"/login"}>Login</Link><br />
+        <Link className="border-white border-2 p-2" href={"/register"}>Register</Link><br />
+      </div>
     </main>
   )
 }
