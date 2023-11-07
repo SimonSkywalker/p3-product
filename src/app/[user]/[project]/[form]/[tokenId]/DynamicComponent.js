3fc11b62c1a6761f11f5c0397d68de675a389a4e
AllanVester <vesterallan246@gmail.com>
2023-11-07 14:06:01 +0100

    Added 'mandatory'-star (*) besides description

--- src/app/[user]/[project]/[form]/[tokenId]/DynamicComponent.js
+++ src/app/[user]/[project]/[form]/[tokenId]/DynamicComponent.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function DynamicComponent({ jsonData }) {
   return (
     <form>
-        <h3>{jsonData.description}</h3>
+        <h3>{jsonData.description}{jsonData.mandatory && <span style={{ color: 'red' }}>*</span>}</h3>
         <div>
           <input className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" type="text" />
         </div>
