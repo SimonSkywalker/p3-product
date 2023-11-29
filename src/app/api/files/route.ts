3b6cb9166bb6a9efe724dea07ada2f7e78742f57
AllanVester <vesterallan246@gmail.com>
2023-11-29 14:58:45 +0100

    Updated route.ts

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -5,23 +5,16 @@ import path from 'path';
 import fs from 'fs';
 
 export async function POST(req: NextRequest) {
-
-    console.log("Fetch caught");
-
     const body = await req.json()
-
-    console.log(body);
     const directoryPath = body.path;
 
     if (typeof directoryPath !== 'string') {
-        console.log("HUUUH")
+        console.log("Invalid directory path")
         return NextResponse.json({ error: 'Invalid directory path' },{status:400});
     }
-    console.log("Body is correct");
 
     try {
         const directory = path.join(process.cwd(), directoryPath);
-        console.log(directory);
         const files = fs.readdirSync(directory);
         return NextResponse.json(files,{status:200});
     } catch (error) {
