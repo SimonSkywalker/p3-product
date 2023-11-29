61ad8a050ad5809882cd72b581e2295bb1e08b2f
AllanVester <vesterallan246@gmail.com>
2023-11-29 15:09:40 +0100

    Updated route.ts

--- src/app/api/writeToFile/route.ts
+++ src/app/api/writeToFile/route.ts
@@ -4,32 +4,26 @@ import fs from 'fs';
 import path from 'path';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-
-    console.log("pee");
     const body = await req.json()
-
-    console.log(body);
     const filePath : string = body.path;
     const data : Array<Object> = body.data;
-    console.log(filePath)
 
     if (typeof filePath !== 'string') {
-        console.log("ERROR BITCH");
+        console.log("Invalid file Path");
         return NextResponse.json({ error: 'Invalid file Path' , status: 400});
     }
     try {
         let file = path.join(process.cwd(), filePath);
-        fs.writeFile(file, JSON.stringify(data, null, "\t"), err => {
+        fs.writeFile(file, JSON.stringify(data), err => {
             if (err) {
                 console.error(err);
+                return NextResponse.json({ error: 'Internal server error', status: 500});
             }
         })
         return NextResponse.json({status: 200});
     }
     catch (error) {
-        console.log("IEEEEEEEEEEEEH")
         console.error(error);
         return NextResponse.json({ error: 'Internal server error', status: 500});
     }
 }
-
