01dadcbe6fbbb7e57e5b9b7afa62c22c1eb03867
Onepl <al03ti@student.aau.dk>
2023-11-29 14:14:34 +0100

    deleted unnecessary files

--- __mocks__/axios.js
+++
@@ -1,9 +0,0 @@
-const mockAxios = jest.genMockFromModule('axios');
-
-mockAxios.create = jest.fn(() => mockAxios);
-
-// Mock other Axios methods as needed
-mockAxios.get = jest.fn();
-mockAxios.post = jest.fn();
-
-export default mockAxios;
