18ca5f22d7d4eb3d0ca2a8977cba7fb7750a426e
sinagaming69 <sgrun22@student.aau.dk>
2023-12-04 12:55:29 +0100

    Everything

--- src/app/[user]/[project]/page.tsx
+++ src/app/[user]/[project]/page.tsx
@@ -11,7 +11,7 @@ import { useState } from 'react';
 import { actionProject, modalOperator } from '@/app/interfaces/interfaces';
 import { Project } from '@/app/components/projectClass';
 import { FormObject } from '@/app/interfaces/interfaces';
-import Form from '@/app/formCreation/form';
+import Form from '@/app/formCreation/Form';
 import FormBuilder from '@/app/formCreation/FormBuilder';
 import Link from 'next/link';
 
