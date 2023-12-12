cc54f359a3ee800dafc5e71c5c0c4e123f92048b
AllanVester <vesterallan246@gmail.com>
2023-12-12 20:37:18 +0100

    Updated register.cy.js

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -13,3 +13,20 @@ describe('Register page', () => {
     cy.get('.Toastify__toast-body > :nth-child(2)').contains('Registered Testing')
   })
 })
+
+describe('Register page', () => {
+  it('Navigates to Register page and creates another user; "Testing"', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.flex-between > .flex-center > :nth-child(2)').click() // Register page
+
+    cy.url().should('eq', 'http://localhost:3000/register');
+
+    cy.get('#username').type('Testing')
+    cy.get('#password').type('Testing')
+    cy.get('#repeatPassword').type('Testing')
+    cy.get('div.mt-2 > .w-full').click() // Register button
+    cy.get('.error').contains('Username already exists')
+
+    cy.url().should('eq', 'http://localhost:3000/register');
+  })
+})
