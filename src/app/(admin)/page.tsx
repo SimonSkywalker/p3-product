c1c7aa75baab32b18e6ce9ecb8dd87073675dcaa
SimonSkywalker <sthordal@gmail.com>
2023-12-13 12:17:32 +0100

    First page redone

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
+      <div className='relative flex flex-col items-center justify-center min-h-screen overflow-hidden'>
       <h1>Welcome to Project survey tool</h1>
-      <div className='flex flex-wrap content-evenly'>
-        <Link className="border-white border-2 p-2" href={"/projectCreation"}>Projects</Link><br />
-        <Link className="border-white border-2 p-2" href={"/login"}>Login</Link><br />
-        <Link className="border-white border-2 p-2" href={"/register"}>Register</Link><br />
+        <div className='flex flex-wrap content-evenly'>
+          This proof of concept was made by cs-23-sw-3-01 on semester 3 AAU Software
+        </div>
       </div>
     </main>
   )
